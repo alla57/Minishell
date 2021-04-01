63a1d1b6eb5ca1e248831aad667493e271209488
Alla eddine BOUMEDIENE <alboumed@e2r11p14.42.fr>
2021-04-01 17:59:43 +0200

    ksjdlfkjsdfkj

--- src/test.c
+++ src/test.c
@@ -6,11 +6,11 @@
 /*   By: alboumed <alboumed@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/31 16:10:08 by alboumed          #+#    #+#             */
-/*   Updated: 2021/03/31 17:53:49 by alboumed         ###   ########.fr       */
+/*   Updated: 2021/04/01 17:59:24 by alboumed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
+#include "../include/minishell.h"
 
 typedef enum	e_op
 {
@@ -34,13 +34,78 @@ typedef struct s_node t_node;
 struct s_node
 {
 	t_token_type	token_type; //facultative
-	t_value			*value;
+	t_value			value;
 	t_node			*left;
 	t_node			*right;
 };
 
-////////////////////////////////////////////
+t_op	get_op(char *str)
+{
+	t_op	op;
+	if (str[0] == '+')
+		op = OP_ADD;
+	else if (str[0] == '*')
+		op = OP_MUL;
+	return (op);
+}
 
+int		token_isdigit(char *str)
+{
+	if (ft_isdigit(str[0]))
+		return (1);
+	return (0);
+}
+
+int		token_isop(char *str)
+{
+	if (str[0] == '+' || str[0] == '*')
+		return (1);
+	return (0);
+}
+
+t_node	*add_node(t_token_type token_type, t_value value)
+{
+	t_node new_node;
+
+	if (!(new_node = malloc(sizeof(t_node))))
+		return (NULL);
+	new_node.token_type = token_type;
+	new_node.value = value;
+	return (&new_node);
+}
+
+t_node	*parser(char **tokens)
+{
+	t_node *node;
+	int i;
+
+	i = 0;
+	if (token_isdigit(token[i]))
+		node = add_node(IS_NUM, ft_atoi(token[i]));
+	else if (token_isop(token[i]))
+		node = add_node(IS_OP, get_op(token[i]));
+	else
+		return (NULL);
+	get_next_token();
+}
+
+char **tokenizer(char *str)
+{
+	if (!str)
+		return (NULL);
+	return (ft_split(str, ' '));
+}
+
+int calcul(char *str)
+{
+	char **stack;
+
+	stack = tokenizer(str);
+	parser();
+}
+
+////////////////////////////////////////////
+/*
 typedef enum	e_op
 {
 	OP_ADD, OP_MUL
@@ -71,3 +136,4 @@ struct	s_node
 	t_node_type		*node_type;
 	t_node			*base;
 };
+*/
