0ab5146dee2c7b6aa022d0db286941c4e4e47759
Alla eddine BOUMEDIENE <alboumed@e2r13p10.42.fr>
2021-04-02 16:55:03 +0200

    dklfjsldfkjsdflkdjfkldjf

--- src/test.c
+++ src/test.c
@@ -6,7 +6,7 @@
 /*   By: alboumed <alboumed@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/31 16:10:08 by alboumed          #+#    #+#             */
-/*   Updated: 2021/04/01 17:59:24 by alboumed         ###   ########.fr       */
+/*   Updated: 2021/04/02 16:54:48 by alboumed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,6 +38,11 @@ struct s_node
 	t_node			*left;
 	t_node			*right;
 };
+
+typedef enum	e_priotity
+{
+	HIGH_PRIORITY, LOW_PRIORITY
+}				t_priority;
 
 t_op	get_op(char *str)
 {
@@ -71,21 +76,80 @@ t_node	*add_node(t_token_type token_type, t_value value)
 		return (NULL);
 	new_node.token_type = token_type;
 	new_node.value = value;
+	node.left = NULL;
+	node.right = NULL;
 	return (&new_node);
 }
 
-t_node	*parser(char **tokens)
+t_node	*add_num_node(t_node *node, char *token)
+{
+	node = add_node(IS_NUM, ft_atoi(token));
+	return (node);
+}
+
+t_node	*add_op_node(t_node *node, char *token)
+{
+	node = add_node(IS_OP, get_op(token));
+	return (node);
+}
+
+t_node	*test(char *token)
 {
 	t_node *node;
+
+	if (token_isdigit(token))
+		add_num_node(node, token);
+	else if (token_isop(token))
+		add_op_node(node, token);
+	return (node);
+}
+
+t_node	*parser(char **token)
+{
+	t_node	*current_node;
+	t_node	*prev_node;
 	int i;
 
 	i = 0;
-	if (token_isdigit(token[i]))
-		node = add_node(IS_NUM, ft_atoi(token[i]));
-	else if (token_isop(token[i]))
-		node = add_node(IS_OP, get_op(token[i]));
-	else
-		return (NULL);
+	// if (token_isdigit(token[i]))
+		add_num_node(current_node, token[i]);
+	// else if (token_isop(token[i]))
+	prev_node = current_node;
+		add_op_node(current_node, token[++i]);
+	current_node->left = prev_node;
+
+//////////////////////////////////////////////////////
+	prev_node = NULL;
+	current_node = test(current_node, token[i]);
+	if (current_node->token_type == IS_OP)
+	{
+		if (current_node->value == OP_MUL)
+		{
+			current_node->left = prev_node;
+			self();
+		}
+		else if (current_node->value == OP_ADD)
+		{
+			
+		}
+		current_node->left = prev_node;
+		current_node->right = 
+	}
+	else if (current_node->token_type == IS_NUM)
+	{
+		if (!token[i + 1])
+			prev_node->right = current_node;
+		else
+			prev_node = current_node;
+	}
+//////////////////////////////////////////////////////
+
+	if (get_op(token[i + 2]) == OP_MUL)
+	{
+		add_op_node(current_node, token[++i]);
+	}
+	// else
+		// return (NULL);
 	get_next_token();
 }
 
